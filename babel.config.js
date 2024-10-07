956f8aae8a3677dda91c2335a88400c2817fda4e
Robin Kroonen <robin@kroonen.ie>
2024-10-06 20:21:47 -0400

    update dependencies and configurations to resolve vulnerabilities

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-babel.config.jsmodule.exports = {
+module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react'
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-class-properties',
     '@babel/plugin-transform-runtime'
   ]
 };
