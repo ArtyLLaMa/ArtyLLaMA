c863e9de5ef62517bdbfac8b50905e2636b7aea8
Robin Kroonen <robin@kroonen.ie>
2024-10-20 22:28:26 -0400

    Remove unnecessary files and update Dockerfile for production deployment

--- dist/assets/channel-DsjeoL9M.js
+++
@@ -1,1 +0,0 @@
-import{aj as o,ak as n}from"./index-BJNGhUTg.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- dist/assets/stateDiagram-v2-7KT6RKTD-aKP1Hf3v.js
+++
@@ -1,1 +0,0 @@
-import{s as t,a as e,c as r,b as s}from"./chunk-5RIWM3LS-DoIJshIO.js";import{_ as i}from"./index-BJNGhUTg.js";import"./chunk-4LC2V6XN-BMIWj6us.js";var _={parser:t,db:e,renderer:r,styles:s,init:i(a=>{a.state||(a.state={}),a.state.arrowMarkerAbsolute=a.arrowMarkerAbsolute,e.clear()},"init")};export{_ as diagram};
